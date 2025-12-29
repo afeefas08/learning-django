ab14ed18e2f28304a8c8ce0006581f7acb582f88
Afeefa <afeefas008@gmail.com>
2025-12-29 14:48:30 +0530

    Creating Category Model for Post.

--- blog/management/commands/populate_posts.py
+++ blog/management/commands/populate_posts.py
@@ -1,6 +1,7 @@
 from typing import Any
-from blog.models import Post
+from blog.models import Post, Category
 from django.core.management.base import BaseCommand
+import random
 
 class Command(BaseCommand):
     help = "This commands inserts post data"
@@ -78,7 +79,9 @@ class Command(BaseCommand):
             "https://picsum.photos/id/20/800/400",
 ]
 
+        categories = Category.objects.all()
         for title,content,img_url in zip(titles,contents, img_urls):
-            Post.objects.create(title = title,content=content,img_url=img_url)
+            category = random.choice(categories)
+            Post.objects.create(title = title,content=content,img_url=img_url, category = category)
 
         self.stdout.write(self.style.SUCCESS("Completed inserting Data!"))
