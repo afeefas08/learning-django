be5d2817a621bfe5a20ac881a36784b146af47a0
Afeefa <afeefas008@gmail.com>
2026-01-01 19:07:06 +0530

    fix: some errors.

--- blog/views.py
+++ blog/views.py
@@ -72,5 +72,9 @@ def contact_view(request):
     return render(request, 'blogs/contact.html')
 
 def about_view(request):
-    about_content = AboutUs.objects.first().content
+    about_content = AboutUs.objects.first()
+    if about_content is None or not about_content.content:
+        about_content = "Default content goes here." # Default text
+    else:
+        about_content = about_content.content
     return render(request, 'blogs/about.html',{'about_content':about_content})
