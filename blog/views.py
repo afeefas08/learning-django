20e0b39a9131f0a91bb37c9b6ddc826a138339c4
Afeefa <afeefas008@gmail.com>
2026-01-02 13:44:18 +0530

    fix: Password Hashing.

--- blog/views.py
+++ blog/views.py
@@ -84,7 +84,9 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save() # user data created
+            user = form.save(commit=False) # user data created
+            user.set_password(form.cleaned_data['password'])  #hashing password.
+            user.save()
             print('Register success!')
 
 
