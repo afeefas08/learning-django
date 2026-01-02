39802224f654e560149ab1d680a806068bc1c108
Afeefa <afeefas008@gmail.com>
2026-01-02 14:09:44 +0530

    feat: Custom Validation & Non Field Errors.

--- blog/forms.py
+++ blog/forms.py
@@ -15,3 +15,12 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username','email','password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Passwords do not match!!')
+        
